570c0638f75cef0fdf3e47975d9831a660829f19
Damlek1 <100087318+Damlek1@users.noreply.github.com>
2022-08-24 19:22:31 +0100

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import NProgress from 'nprogress'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import Layout from '../components/Layout'
-import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
   NProgress.configure({showSpinner: false})
@@ -18,7 +17,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+       integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+       crossOrigin="anonymous" referrerPolicy="no-referrer" />
     </Head>
     <ChakraProvider>
       <Layout>
